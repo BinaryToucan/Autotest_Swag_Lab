a2567f8bce459033c56b625571703bf40a309307
Emiliya Melnikova <binarytoucan@gmail.com>
2026-06-11 13:23:43 +0300

    update todos tests

--- src/test/java/api/tests/TodoTests.java
+++ src/test/java/api/tests/TodoTests.java
@@ -2,34 +2,52 @@ package api.tests;
 
 import api.clients.TodoClient;
 import api.models.Todo;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import annotations.ApiTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.hasKey;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ApiTest
 public class TodoTests {
-    TodoClient todoClient = new TodoClient();
+    private static final int EXPECTED_TODOS_COUNT = 200;
+    private static final int EXISTING_TODO_ID = 1;
 
+    private final TodoClient todoClient = new TodoClient();
+
+    @DisplayName("Should return 200 for existing todo")
     @Test
-    void checkSingleTodo() {
-        Todo todo =
+    void checkValidSingleTodo() {
+        var todo =
                 todoClient
-                        .getTodo(1)
+                        .getTodo(EXISTING_TODO_ID)
                         .then()
                         .statusCode(200)
                         .extract()
                         .as(Todo.class);
 
-        assertEquals(1, todo.getId());
-        assertFalse(todo.isCompleted());
+        assertAll(
+                () -> assertEquals(EXISTING_TODO_ID, todo.getId()),
+                () -> assertFalse(todo.isCompleted())
+        );
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 201})
+    @DisplayName("Should return 404 for invalid todo")
+    void checkTodoWithInvalidId(int id) {
+        todoClient
+                .getTodo(id)
+                .then()
+                .statusCode(404);
+    }
+
+    @DisplayName("Should return all todos")
     @Test
     void checkTodosCount() {
 
@@ -42,26 +60,58 @@ public class TodoTests {
                         .jsonPath()
                         .getList("", Todo.class);
 
-        assertEquals(200, todos.size());
+        assertEquals(EXPECTED_TODOS_COUNT, todos.size());
     }
 
     @Test
-    void checkTitleContains() {
-
-        todoClient
+    @DisplayName("Should map todo correctly")
+    void shouldMapTodoCorrectly() {
+        Todo todo = todoClient
                 .getTodo(1)
                 .then()
                 .statusCode(200)
-                .body("title", containsString("delectus"));
+                .extract()
+                .as(Todo.class);
+
+        assertAll(
+                () -> assertEquals(1, todo.getUserId()),
+                () -> assertEquals(1, todo.getId()),
+                () -> assertEquals("delectus aut autem", todo.getTitle()),
+                () -> assertFalse(todo.isCompleted())
+        );
     }
 
     @Test
-    void checkCompletedFieldExists() {
+    @DisplayName("Should return identical todo for repeated requests")
+    void shouldBeEqual() {
+        Todo first = todoClient.getTodo(EXISTING_TODO_ID)
+                .as(Todo.class);
 
-        todoClient
-                .getTodo(1)
+        Todo second = todoClient.getTodo(EXISTING_TODO_ID)
+                .as(Todo.class);
+
+        assertEquals(first, second);
+    }
+
+    @ParameterizedTest
+    @MethodSource("todos")
+    @DisplayName("Todo should contain all required fields")
+    void shouldContainAllRequiredFields(Todo todo) {
+        assertAll(
+                () -> assertTrue(todo.getUserId() > 0),
+                () -> assertTrue(todo.getId() > 0),
+                () -> assertNotNull(todo.getTitle()),
+                () -> assertFalse(todo.getTitle().isBlank())
+        );
+    }
+
+    static Stream<Todo> todos() {
+        return new TodoClient()
+                .getAllTodos()
                 .then()
-                .statusCode(200)
-                .body("$", hasKey("completed"));
+                .extract()
+                .jsonPath()
+                .getList("", Todo.class)
+                .stream();
     }
 }
